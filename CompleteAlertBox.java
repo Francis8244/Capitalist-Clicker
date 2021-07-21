9fc12f4c0555751bf325a7d83d1b6005dcfa23a8
Francis8244 <60706869+Francis8244@users.noreply.github.com>
2021-07-21 04:41:44 -0500

    Update CompleteAlertBox.java

--- CompleteAlertBox.java
+++ CompleteAlertBox.java
@@ -1,6 +1,4 @@
 package application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.stage.Modality;
@@ -14,23 +12,28 @@ import javafx.scene.text.Font;
 public class CompleteAlertBox {
 	
 	public static void display (String title, String message) {
+		//Creates the stage and makes sure the user can't click out the window
 		Stage stage = new Stage();
 		stage.initModality(Modality.APPLICATION_MODAL);
 		stage.setTitle(title);
 		stage.setMinWidth(100);
 		stage.setMinHeight(50);;
 		
+		//Initializing the label text for the message the window is displaying
 		Label label = new Label();
 		label.setText(message);
 		label.setFont(new Font("Arial", 20));
 		
+		//Initializing the close window button
 		Button closeButton = new Button("Close");
 		closeButton.setOnAction(e -> stage.close());
 		
+		//Vertically aligns the label text and button
 		VBox align = new VBox();
 		align.getChildren().addAll(label, closeButton);
 		align.setAlignment(Pos.CENTER);
-
+		
+		//Adds scene to the stage
 		Scene scene = new Scene(align);
 		stage.setScene(scene);
 		stage.showAndWait();
